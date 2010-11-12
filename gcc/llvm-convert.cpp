431876bcfbe975fd37df8776393272f00ff599c1
David Meyer <pdox@google.com>
2010-11-12 13:41:24 -0800

    Updating vendor to r118926

--- gcc/llvm-convert.cpp
+++ gcc/llvm-convert.cpp
@@ -4922,14 +4922,16 @@ Value *TreeToLLVM::EmitASM_EXPR(tree exp) {
       StoreCallResultAddrs.push_back(Dest.Ptr);
       ConstraintStr += ",=";
       ConstraintStr += SimplifiedConstraint;
-      CallResultTypes.push_back(DestValTy);
+      CallResultTypes.push_back(LLVM_ADJUST_MMX_INLINE_PARAMETER_TYPE(
+                                SimplifiedConstraint, DestValTy));
       CallResultIsSigned.push_back(!TYPE_UNSIGNED(TREE_TYPE(Operand)));
       OutputLocations.push_back(std::make_pair(true, CallResultTypes.size()-1));
     } else {
       ConstraintStr += ",=*";
       ConstraintStr += SimplifiedConstraint;
       CallOps.push_back(Dest.Ptr);
-      CallArgTypes.push_back(Dest.Ptr->getType());
+      CallArgTypes.push_back(LLVM_ADJUST_MMX_INLINE_PARAMETER_TYPE(
+                                SimplifiedConstraint, Dest.Ptr->getType()));
       OutputLocations.push_back(std::make_pair(false, CallArgTypes.size()-1));
     }
   }
@@ -5051,8 +5053,12 @@ Value *TreeToLLVM::EmitASM_EXPR(tree exp) {
         }
       }
 
+      const Type* AdjTy = LLVM_ADJUST_MMX_INLINE_PARAMETER_TYPE(
+                              Constraint, Op->getType());
+      if (AdjTy != Op->getType())
+        Op = BitCastToType(Op, AdjTy);
       CallOps.push_back(Op);
-      CallArgTypes.push_back(Op->getType());
+      CallArgTypes.push_back(AdjTy);
     } else {                          // Memory operand.
       lang_hooks.mark_addressable(TREE_VALUE(Input));
       isIndirect = true;
@@ -5156,11 +5162,20 @@ Value *TreeToLLVM::EmitASM_EXPR(tree exp) {
   CV->setDoesNotThrow();
 
   // If the call produces a value, store it into the destination.
-  if (StoreCallResultAddrs.size() == 1)
-    Builder.CreateStore(CV, StoreCallResultAddrs[0]);
-  else if (unsigned NumResults = StoreCallResultAddrs.size()) {
+  if (StoreCallResultAddrs.size() == 1) {
+    Value *V = CV;
+    const Type *DestValTy =
+      cast<PointerType>(StoreCallResultAddrs[0]->getType())->getElementType();
+    if (CV->getType() != DestValTy)
+      V = BitCastToType(CV, DestValTy);
+    Builder.CreateStore(V, StoreCallResultAddrs[0]);
+  } else if (unsigned NumResults = StoreCallResultAddrs.size()) {
     for (unsigned i = 0; i != NumResults; ++i) {
+      const Type *DestValTy =
+        cast<PointerType>(StoreCallResultAddrs[i]->getType())->getElementType();
       Value *ValI = Builder.CreateExtractValue(CV, i, "asmresult");
+      if (ValI->getType() != DestValTy)
+        ValI = BitCastToType(ValI, DestValTy);
       Builder.CreateStore(ValI, StoreCallResultAddrs[i]);
     }
   }
@@ -5328,7 +5343,7 @@ TreeToLLVM::BuildBinaryAtomicBuiltin(tree exp, Intrinsic::ID id) {
   // The gcc builtins are also full memory barriers.
   // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-  EmitMemoryBarrier(true, true, true, true, false);
+  EmitMemoryBarrier(false, false, true, true, false);
 #else
   EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -5371,7 +5386,7 @@ TreeToLLVM::BuildCmpAndSwapAtomicBuiltin(tree exp, tree type, bool isBool) {
   // The gcc builtins are also full memory barriers.
   // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-  EmitMemoryBarrier(true, true, true, true, false);
+  EmitMemoryBarrier(false, false, true, true, false);
 #else
   EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -5904,7 +5919,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -5951,7 +5966,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -5998,7 +6013,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -6045,7 +6060,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -6092,7 +6107,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
@@ -6139,7 +6154,7 @@ bool TreeToLLVM::EmitBuiltinCall(tree exp, tree fndecl,
     // The gcc builtins are also full memory barriers.
     // FIXME: __sync_lock_test_and_set and __sync_lock_release require less.
 #if defined(TARGET_ARM) && defined(CONFIG_DARWIN_H)
-    EmitMemoryBarrier(true, true, true, true, false);
+    EmitMemoryBarrier(false, false, true, true, false);
 #else
     EmitMemoryBarrier(true, true, true, true, true);
 #endif
